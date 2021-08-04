ee261868bc984ef195061397eba7165b9afe047b
OsvaldoEmanuel <osvaldozakowicz@gmail.com>
2021-08-04 13:59:37 -0300

    lista la interactividad para cambiar la cita de forma dinamica

--- app.js
+++ app.js
@@ -1,8 +1,39 @@
+/* indice sobre las citas */
+let indexSelectedQuote = 0;
+/* lista de autores */
 const authorsList = document.querySelectorAll('.author-btn');
+/* lista de citas */
 const quotesList = document.querySelectorAll('.quote');
 
+/* agregar eventos */
+authorsList.forEach((author)=>{
+  author.addEventListener('click', handleClick);
+});
+
+/**
+ * manejador de evento click para obtener el data index del boton
+ * @param {*} event 
+ */
 function handleClick(event) {
-  const selectedQuote = parseInt(event.currentTarget.dataset.index, 10);
-  showQuote(selectedQuote);
+  const indexSelectedAuthor = parseInt(event.currentTarget.dataset.index, 10);
+  showQuote(indexSelectedAuthor);
+}
+
+/**
+ * mostrar cita asociada al autor
+ * @param {*} indexSelectedAuthor 
+ * @returns 
+ */
+function showQuote(indexSelectedAuthor) {
+  if(indexSelectedQuote === indexSelectedAuthor) {
+    //autor elegido === cita visible
+    return;
+  } else {
+    //autor elegido !== cita visible => hacer visible la cita
+    quotesList[indexSelectedQuote].classList.toggle('quote-hidden');
+    quotesList[indexSelectedAuthor].classList.toggle('quote-hidden');
+    //actualizar el index
+    indexSelectedQuote = indexSelectedAuthor; 
+  }
 }
 
